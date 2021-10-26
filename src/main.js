38e4b4b3e62175bf4259f912537b2a319ed56fe1
Seung1110 <iiwvii@naver.com>
2021-10-26 13:49:12 +0900

    주석 추가

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+/*index.html 실행 후 실행 */
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false  // 개발 중 나타는 hint 기능 막기
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({ // Vue 객체 생성
+  router,
+  render: h => h(App)
+}).$mount('#app') // index.html의 <div id='app'>에 App.vue 내용 추가
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue' // views 폴더에 존재하는 vue 파일 추가
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home', 
+    component: Home // path 이동 시 어떤 View를 보여줄 것인지
+  }, // 하나의 경로에 하나의 객체
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue') // 사용자가 요청 시 vue 파일 로딩
+    // component: About // 접근과 동시에 로딩, import About from '../views/About.vue' 추가 필요
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
