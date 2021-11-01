507ef3ba0dd1e3eacd626ed8305925419b3c3680
Seung1110 <iiwvii@naver.com>
2021-11-01 17:03:28 +0900

    강의 내용 정리

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false  // 개발 중 나타는 hint 기능 막기
 
-new Vue({ // Vue 객체 생성
+new Vue({
+  // Vue 객체 생성
   router,
-  render: h => h(App) // 비동기
+
+  store,
+
+  // 비동기
+  render: h => h(App)
 }).$mount('#app') // mount 시점에 index.html의 <div id='app'>에 App.vue 내용 추가(component를 렌더링)
